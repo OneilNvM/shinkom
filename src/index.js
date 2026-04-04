dd11d0c5b6808893b77e9e093d793f10be25f310
Oneil Achord <oneil324@outlook.com>
2026-04-04 18:40:00 +0100

    chore: Added `UIComponent` to the export from the core files.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 export * from './ui/index'
 export * from './engine/index'
 export * from './shinkom'
-export { ShinkomBus, ShinkomState } from './core'
+export { ShinkomBus, ShinkomState, UIComponent } from './core'
