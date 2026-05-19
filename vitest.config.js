461fc8870646d75e437741b5c943f25790f239fc
Oneil Achord <oneil324@outlook.com>
2026-05-19 03:07:47 +0100

    chore: add setup files for localstorage, stylesheets, and webassembly in Vitest configuration.

--- vitest.config.js
+++ vitest.config.js
@@ -2,8 +2,26 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
     test: {
-        environment: 'jsdom',
-        setupFiles: ['./tests/setup_files/engine.setup.ts'],
+        environment: 'node',
+        projects: [
+            {
+                extends: true,
+                test: {
+                    name: 'ui',
+                    environment: 'jsdom',
+                    include: ['tests/client/**/*.test.js'],
+                    setupFiles: ['tests/setup_files/stylesheets.ts']
+                }
+            },
+            {
+                test: {
+                    name: 'engine',
+                    include: ['tests/engine/**/*.test.js'],
+                    setupFiles: ['tests/setup_files/webassembly.ts', 'tests/setup_files/stylesheets.ts']
+                }
+            }
+        ],
+        setupFiles: ['./tests/setup_files/webassembly.ts', './tests/setup_files/stylesheets.ts'],
         globals: true
     }
 })
