bac52d37762c5c25a036dcbaaada9f440bb831c2
Oneil Achord <oneil324@outlook.com>
2026-05-27 17:18:45 +0100

    feat: exporting custom elements for the inspector, control panel, and compatibility view from index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 export * from './ui/index'
 export * from './engine/index'
 export * from './shinkom'
-export { ShinkomBus, ShinkomState, UIComponent } from './core'
+export { ShinkomBus, ShinkomState, UIComponent, CompatInspectorElement, CompatControlPanelElement, CompatViewElement } from './core'
