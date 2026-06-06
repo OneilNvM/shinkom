c61a21f8d44d108803b4ecba7620ed175742076d
Oneil Achord <oneil324@outlook.com>
2026-06-06 18:37:10 +0100

    tests: fixed rust engine test file path error

--- crates/shinkore/tests/engine_tests.rs
+++ crates/shinkore/tests/engine_tests.rs
@@ -11,9 +11,9 @@ fn should_create_engine() -> Result<(), Box<dyn Error>> {
     use std::fs::File;
     use std::io::Read;
 
-    let mut compat_data_file = File::open("../gen/compat-data.json").unwrap();
-    let mut browser_data_file = File::open("../gen/browser-data.json").unwrap();
-    let mut browser_usage_data_file = File::open("../gen/browser-usage-data.json").unwrap();
+    let mut compat_data_file = File::open("../../packages/shinkom/gen/compat-data.json").unwrap();
+    let mut browser_data_file = File::open("../../packages/shinkom/gen/browser-data.json").unwrap();
+    let mut browser_usage_data_file = File::open("../../packages/shinkom/gen/browser-usage-data.json").unwrap();
     let mut compat_data = String::new();
     let mut browser_data = String::new();
     let mut browser_usage_data = String::new();
