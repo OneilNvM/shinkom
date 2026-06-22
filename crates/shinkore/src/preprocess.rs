8841e9a8fccbff08a94997a00c46af580b837006
Oneil Achord <oneil324@outlook.com>
2026-06-22 01:54:24 +0100

    feat: added divan as a dev dependency and implement preprocessing benchmarks for HTML inputs
    - Improved performance of pre_process_html based on benchmark results.

--- crates/shinkore/src/preprocess.rs
+++ crates/shinkore/src/preprocess.rs
@@ -1,12 +1,19 @@
 //! This module contains functions for pre-processing HTML input before compatibility checks.
 //! The purpose of these functions is to make sure that the HTML input passed into
 //! compatibility checking functions is standardized to output accurate results.
-use std::{cell::RefCell, collections::HashMap, rc::Rc};
+use std::{cell::RefCell, rc::Rc};
 
 use lol_html::{RewriteStrSettings, element, html_content::Element, rewrite_str};
 use regex::Regex;
 
 use crate::{constants::IGNORE_TAGS, errors::PreProcessError};
+
+#[derive(Debug, Clone, Copy)]
+enum PreProcessState {
+    Active,
+    Ignoring,
+    Truncating,
+}
 
 /// Used for pre-processing HTML to return all of the elements down to the specified `depth_level`,
 /// which **must be greater than 0**.
@@ -54,7 +61,7 @@ use crate::{constants::IGNORE_TAGS, errors::PreProcessError};
 /// Notice how the `<p>` tags in the `inner-div` were not returned.
 pub fn pre_process_html(html: &str, depth_level: u32) -> String {
     let mut result: Vec<&str> = vec![];
-    let mut lines = html.lines();
+    let mut lines = html.trim().lines();
 
     // Handle base cases
     if depth_level == 0 {
@@ -75,100 +82,88 @@ pub fn pre_process_html(html: &str, depth_level: u32) -> String {
     // Determines when an open tag is present at the depth level
     let mut parent = false;
 
-    // Ignores any line until the parent element's close tag has been reached
-    let mut ignore_until_closed = false;
+    let mut state = PreProcessState::Active;
 
-    let mut cur_line = lines.next();
-    while cur_line.is_some() {
-        let line = cur_line.unwrap();
-
+    for line in lines {
         // Reset `parent` if parent is true
         // and when current depth is less than depth level
         if cur_depth < depth_level && parent {
             parent = false;
         }
 
-        // Ignore current line except for when the line is equal
-        // to the last String in `close_tags`
-        if ignore_until_closed {
-            if let Some(close) = close_tags.last()
-                && line == close
-            {
-                cur_depth -= 1;
-                close_tags.pop();
-                ignore_until_closed = false;
+        match state {
+            PreProcessState::Ignoring => {
+                // Ignore current line except for when the line is equal
+                // to the last String in `close_tags`
+                if let Some(close) = close_tags.last()
+                    && line == close
+                {
+                    cur_depth -= 1;
+                    close_tags.pop();
+                    state = if cur_depth >= depth_level {
+                        PreProcessState::Truncating
+                    } else {
+                        PreProcessState::Active
+                    }
+                }
             }
-            cur_line = lines.next();
-            continue;
-        }
-
-        // Handle situations when current depth is greater than or equal to depth level
-        if cur_depth >= depth_level {
-            // Decrement `cur_depth` and remove a close tag if the current line matches the last close tag
-            if line == close_tags.last().unwrap() {
-                cur_depth -= 1;
-                close_tags.pop();
-                cur_line = lines.next();
-                continue;
-            } else {
-                // If parent is already true and current line is an open tag,
-                // increase `cur_depth` and push the close tag
-                if parent && let Some(close_tag) = write_close_tag(line) {
+            PreProcessState::Truncating => {
+                // Decrement `cur_depth` and remove a close tag if the current line matches the last close tag
+                if line == *close_tags.last().unwrap() {
+                    cur_depth -= 1;
+                    close_tags.pop();
+                    if cur_depth < depth_level {
+                        state = PreProcessState::Active;
+                    }
+                } else if parent && let Some(close_tag) = write_close_tag(line) {
+                    // If parent is already true and current line is an open tag,
+                    // increase `cur_depth` and push the close tag
                     cur_depth += 1;
                     close_tags.push(close_tag);
                 }
-
-                cur_line = lines.next();
-                continue;
             }
-        }
-
-        // Decrement `cur_depth` if line is a cloes tag and matches last close tag
-        if line.starts_with("</") {
-            if cur_depth > 0 && line == close_tags.last().unwrap() {
-                cur_depth -= 1;
-                close_tags.pop();
-            }
-
-            cur_line = lines.next();
-            continue;
-        }
-
-        // Check if the current line is an open tag
-        if let Some(close_tag) = write_close_tag(line) {
-            // Ignore any content within ignored tags in next iteration
-            if IGNORE_TAGS.contains(&close_tag.as_str()) {
-                cur_depth += 1;
-                close_tags.push(close_tag);
-                result.push("\n");
-                result.push(line);
-                ignore_until_closed = true;
-            } else {
-                cur_depth += 1;
-                close_tags.push(close_tag);
-
-                // Push line to result if current depth is less than depth level
-                if cur_depth < depth_level {
-                    result.push("\n");
-                    result.push(line);
-                } else {
-                    // Push line to result if depth level is equal to current depth
-                    if depth_level == cur_depth {
+            PreProcessState::Active => {
+                // Decrement `cur_depth` if line is a cloes tag and matches last close tag
+                if line.starts_with("</") {
+                    if cur_depth > 0 && line == *close_tags.last().unwrap() {
+                        cur_depth -= 1;
+                        close_tags.pop();
+                    }
+                } else if let Some(close_tag) = write_close_tag(line) {
+                    // Ignore any content within ignored tags in next iteration
+                    if IGNORE_TAGS.contains(&close_tag.as_str()) {
+                        cur_depth += 1;
+                        close_tags.push(close_tag);
                         result.push("\n");
                         result.push(line);
+                        state = PreProcessState::Ignoring;
+                    } else {
+                        cur_depth += 1;
+                        close_tags.push(close_tag);
+
+                        // Push line to result if current depth is less than depth level
+                        if cur_depth < depth_level {
+                            result.push("\n");
+                            result.push(line);
+                        } else {
+                            // Push line to result if depth level is equal to current depth
+                            if depth_level == cur_depth {
+                                result.push("\n");
+                                result.push(line);
+                            }
+                            // Set parent to true to flag that the parent element was reached
+                            if cur_depth >= depth_level && !parent {
+                                parent = true;
+                                state = PreProcessState::Truncating;
+                            }
+                        }
                     }
-                    // Set parent to true to flag that the parent element was reached
-                    if !parent {
-                        parent = true
-                    }
+                } else {
+                    result.push("\n");
+                    result.push(line);
                 }
             }
-        } else {
-            result.push("\n");
-            result.push(line);
         }
-
-        cur_line = lines.next();
     }
 
     result.into_iter().collect()
@@ -202,8 +197,8 @@ pub fn pre_process_html(html: &str, depth_level: u32) -> String {
 /// }
 /// ```
 pub fn write_close_tag(line: &str) -> Option<String> {
-    let end_tag = Rc::new(RefCell::new(None));
-    let open_tags: Rc<RefCell<HashMap<String, String>>> = Rc::new(RefCell::new(HashMap::new()));
+    let mut end_tag: Option<String> = None;
+    let open_tags: Rc<RefCell<Vec<String>>> = Rc::new(RefCell::new(Vec::new()));
 
     // Return the equivalent end tag if the line has an open tag
     let _ = rewrite_str(
@@ -211,29 +206,22 @@ pub fn write_close_tag(line: &str) -> Option<String> {
         RewriteStrSettings::new().append_element_content_handler(element!(
             "*",
             |el: &mut Element| {
-                let open_tags_inner = Rc::clone(&open_tags);
-                let end_tag_inner = Rc::clone(&end_tag);
-
+                let inner_open_tags = Rc::clone(&open_tags);
                 // Check if the element is an open tag
                 if el.can_have_content() {
-                    open_tags_inner
-                        .borrow_mut()
-                        .insert(el.tag_name(), format!("</{}>", el.tag_name()));
-                    *end_tag.borrow_mut() = Some(format!("</{}>", el.tag_name()));
+                    let tag_name = el.tag_name();
+                    inner_open_tags.borrow_mut().push(tag_name.clone());
+                    end_tag = Some(tag_name);
                 }
 
                 // Check if the line has a close tag
                 if let Some(handlers) = el.end_tag_handlers() {
                     handlers.push(Box::new(move |end| {
-                        if open_tags_inner.borrow().contains_key(&end.name()) {
-                            open_tags_inner.borrow_mut().remove(&end.name());
-                            match open_tags_inner.borrow().iter().last() {
-                                Some((_, tag)) => {
-                                    *end_tag_inner.borrow_mut() = Some(tag.to_owned())
-                                }
-                                None => *end_tag_inner.borrow_mut() = None,
-                            }
+                        let mut live_tags = inner_open_tags.borrow_mut();
+                        if let Some(pos) = live_tags.iter().rposition(|tag| *tag == end.name()) {
+                            live_tags.remove(pos);
                         }
+
                         Ok(())
                     }))
                 }
@@ -243,7 +231,11 @@ pub fn write_close_tag(line: &str) -> Option<String> {
         )),
     );
 
-    end_tag.borrow_mut().take()
+    if !open_tags.borrow().is_empty() {
+        end_tag.map(|name| format!("</{name}>"))
+    } else {
+        None
+    }
 }
 
 /// Format the tags in HTML string to be on individual lines.
@@ -252,9 +244,9 @@ pub fn write_close_tag(line: &str) -> Option<String> {
 ///
 /// ```rust
 /// use shinkore::preprocess::format_html;
-/// use wasm_bindgen::JsError;
+/// use shinkore::errors::PreProcessError;
 ///
-/// fn main() -> Result<(), JsError> {
+/// fn main() -> Result<(), PreProcessError> {
 ///
 /// let html = "<aside><section><h2>Aside Title</h2><div>Content</div></section></aside>";
 ///
